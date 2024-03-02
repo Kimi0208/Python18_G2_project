672751e0ffad161c0c5b3411c24e98ae90b3790d
Dmitrii <funmaker04@gmail.com>
2024-03-02 14:35:20 +0600

    Немного изменил модель пользователя в админке, чтобы пароль в бд записывался зашифрованный. Также подкорректировал шаблон для документов.
    Также написал тестовую функцию которая отправляет оповещение на почтовый адрес. Пока никуда не добавлял этот функционал.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,43 @@
 from django.contrib import admin
 from accounts.models import Department, Position, DefUser
+from django.contrib.auth.admin import UserAdmin
 
 
 admin.site.register(Department)
 admin.site.register(Position)
-admin.site.register(DefUser)
+
+
+class CustomUserAdmin(UserAdmin):
+    model = DefUser
+    list_display = ('username', 'first_name', 'email', 'phone_number')
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (("Personal info"), {"fields": ('first_name', 'last_name', 'patronymic', 'email', 'phone_number', 'position')}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ('username', 'first_name', 'last_name', 'patronymic', 'password1', 'password2', 'email',
+                           'phone_number', 'position'),
+            },
+        ),
+    )
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+
+
+admin.site.register(DefUser, CustomUserAdmin)
---
+++ webapp/views/test_mail_send.py
@@ -0,0 +1,35 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def send_email(subject, message, from_email, to_email, smtp_server, smtp_port, smtp_username, smtp_password):
+    msg = MIMEMultipart()
+
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message, 'plain'))
+
+    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+        server.login(smtp_username, smtp_password)
+        server.send_message(msg)
+
+ttt = 'CRM:  '
+
+subject = ttt + "Тема"
+message = "Тело письма"
+from_email = "от кого"
+to_email = "кому"
+smtp_server = "сервер"
+smtp_port = 465
+smtp_username = 'логин'
+smtp_password = "пароль"
+
+
+
+try:
+    send_email(subject, message, from_email, to_email, smtp_server, smtp_port, smtp_username, smtp_password)
+except Exception as e:
+    print("Произошла ошибка при отправке email:", e)
