bd451111e1f85254785bd8035f9ffc27f5d09584
ikim <Kimig0208@gmail.com>
2024-03-20 15:11:21 +0600

    Добавил модели для работы с делопроизводствои и дописал функции сортировки файлов по папкам, сделал и применил миграции

--- webapp/models.py
+++ webapp/models.py
@@ -61,7 +61,7 @@ class Priority(models.Model):
 
 
 class File(models.Model):
-    file = models.FileField(verbose_name="Файлы", upload_to='uploads/user_docs', null=True, blank=True)
+    file = models.FileField(verbose_name="Файлы", upload_to='uploads/', null=True, blank=True)
     user = models.ForeignKey('accounts.DefUser', on_delete=models.CASCADE, verbose_name='От кого', null=True,
                              blank=True)
     task = models.ForeignKey('Task', on_delete=models.CASCADE, verbose_name='Задача', null=True, blank=True)
@@ -74,3 +74,79 @@ class Checklist(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class InOutMailsStatus(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Статус')
+
+
+class InOutMailsType(models.Model):
+    type = models.CharField(max_length=50, verbose_name='Тип')
+
+
+def upload_path_for_mails(instance, filename):
+    if instance.type.type == 'входящее письмо':
+        return 'uploads/mails/input_mails/{0}'.format(filename)
+    elif instance.type.type == 'исходящее письмо':
+        return 'uploads/mails/output_mails/{0}'.format(filename)
+
+
+class InOutMails(models.Model):
+    mail_number = models.CharField(auto_created=True, max_length=250, verbose_name='Порядковый номер документа',
+                                   null=True, blank=True)
+    registration_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата/Время регистрации документа')
+    input_mail_number = models.CharField(max_length=250, verbose_name='Номер исходящего письма отправителя',
+                                         null=True, blank=True)
+    sender_name = models.CharField(max_length=250, verbose_name='Наименование отправителя', null=True, blank=True)
+    mail_description = models.TextField(max_length=250, verbose_name='Тема сообщения', null=True, blank=True)
+    pages_count = models.IntegerField(default=0, verbose_name='Количество страниц', null=True, blank=True)
+    responsible_department = models.ForeignKey('accounts.Department', on_delete=models.CASCADE,
+                                               verbose_name='Ответственный отдел', null=True, blank=True)
+    responsible_employee = models.ForeignKey('accounts.Position', on_delete=models.CASCADE,
+                                             verbose_name='Ответственный сотрудник', null=True, blank=True)
+    file = models.ForeignKey(File, on_delete=models.CASCADE, verbose_name='Вложение', null=True, blank=True)
+    initiator_of_mail = models.ForeignKey('accounts.DefUser', on_delete=models.CASCADE,
+                                          verbose_name='Инициатор исходящего письма')
+    status = models.ForeignKey('InOutMailsStatus', on_delete=models.CASCADE, verbose_name='Статус письма')
+    type = models.ForeignKey('InOutMailsType', on_delete=models.CASCADE, verbose_name='Тип письма')
+
+
+def upload_path_for_companies_list(instance, filename):
+    if instance.company_code:
+        return 'uploads/companies_list/{}/{}'.format(instance.company_code, filename)
+
+
+class CompaniesList(models.Model):
+    company_code = models.CharField(max_length=250, verbose_name='Код компании')
+    company_name = models.CharField(max_length=250, verbose_name='Назыание компании')
+    contract_with_company = models.ForeignKey('File', on_delete=models.CASCADE, verbose_name='Договор с компанией',
+                                              null=True, blank=True)
+    company_inn = models.CharField(max_length=250, verbose_name='ИНН Компании', null=True, blank=True)
+
+
+class ContractLocation(models.Model):
+    location_name = models.CharField(max_length=250, verbose_name='Физическое расположение договора')
+
+
+def upload_path_for_contracts(instance, filename):
+    if instance.company:
+        return 'uploads/contracts/{}/{}'.format(instance.company.company_code, filename)
+    else:
+        return 'uploads/contracts/unknown/{}'.format(filename)
+
+
+class ContractRegistry(models.Model):
+    company = models.ForeignKey('CompaniesList', on_delete=models.CASCADE, verbose_name='Компания', null=True,
+                                blank=True)
+    input_contract_number = models.CharField(max_length=250, verbose_name='Порядковый номер входяшего документа',
+                                             null=True, blank=True)
+    description = models.TextField(max_length=250, verbose_name='Описание документа', null=True, blank=True)
+    consultion_date = models.DateField(auto_now_add=False, verbose_name='Дата заключения договора', null=True,
+                                       blank=True)
+    responsible_employee = models.ForeignKey('accounts.DefUser', on_delete=models.CASCADE,
+                                             verbose_name='Ответственный сотрудник', null=True, blank=True)
+    scan_copy = models.BooleanField(default=False, verbose_name='Наличие отсканированной копии')
+    file = models.ForeignKey('File', on_delete=models.CASCADE, verbose_name='Вложение', null=True,
+                             blank=True)
+    contract_location = models.ForeignKey('ContractLocation', on_delete=models.CASCADE,
+                                          verbose_name='Физическое расположение договора')
